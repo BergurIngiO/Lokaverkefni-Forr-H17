c67b068d9db5e0d46d39426a717cd4033e88c5b7
BerugrIngiO <bergurol342@nemi.tskoli.is>
2017-11-22 16:43:21 +0000

    Adda bilar class

---
+++ Desktop/FOR2TO05CU/hópverkefni/hópverkefni.py
@@ -0,0 +1,12 @@
+#Bergur Ingi Ólafsson
+#Kristberg Rúnar
+#Hópverkefni í forritun Haust-Önn 2017
+#22.11.2017
+
+class Bilar:
+    def __init__(self, skraningarnumer, argerd, gerdbils, athugasemd):
+        self.skraningarnumer = skraningarnumer
+        self.argerd = argerd
+        self.gerdbils = gerdbils
+        self.athugasemd = athugasemd
+
